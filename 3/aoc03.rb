57b122faf2eadf547f4ca1af6597e454ff1576d5
Steven Brown <sgbrown@mac.com>
2016-01-04 10:39:04 +1300

    day 3, part 1 done

--- 3/aoc03.rb
+++ 3/aoc03.rb
@@ -1,4 +1,14 @@
+require 'set'
+Point = Struct.new(:x, :y)
+current_x = 0
+current_y = 0
+locations = Set.new [Point.new(current_x,current_y)]
 f = File.new("input03.txt")
 f.each_char do |c|
-print c, ' '
+  current_x -= 1 if c == '<'
+  current_x += 1 if c == '>'
+  current_y -= 1 if c == '^'
+  current_y += 1 if c == 'v'
+  locations.add(Point.new(current_x,current_y))
 end
+puts locations.count
