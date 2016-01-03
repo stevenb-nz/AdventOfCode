fcbb73931ada9dea0e049216716e233b9e3c69b8
Steven Brown <sgbrown@mac.com>
2016-01-03 22:37:13 +1300

    day 2 finished

--- 2/aoc02.rb
+++ 2/aoc02.rb
@@ -1,4 +1,10 @@
 f = File.new("input02.txt")
+total = 0
+ribbon_total = 0
 f.each do |line|
-  puts line
+  la = line.chomp.split('x').map(&:to_i).sort
+  total += 3*la[0]*la[1]+2*la[2]*(la[0]+la[1])
+  ribbon_total += 2*(la[0]+la[1])+la[0]*la[1]*la[2]
 end
+puts total
+puts ribbon_total
