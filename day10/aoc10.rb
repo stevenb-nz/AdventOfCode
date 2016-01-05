27b5c824a75c94903fe334c576bacaaa79fbb466
Steven Brown <sgbrown@mac.com>
2016-01-06 12:24:07 +1300

    day 10

--- day10/aoc10.rb
+++ day10/aoc10.rb
@@ -1,5 +1,16 @@
-f = File.new("input10.txt")
-f.each_char do |c|
-print c
+def look_say input
+  output = input.scan(/((\d)\2*)/).map(&:first).each_with_object "" do |sc, str|
+      str << "#{sc.length}#{sc[0]}"
+    end
+  output
 end
-puts
+
+f = File.new("input10.txt")
+input = ""
+f.each_char do |c|
+  input += c
+end
+40.times { input = look_say(input) }
+puts input.length
+10.times { input = look_say(input) }
+puts input.length
